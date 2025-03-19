a3d8bf4233e24b86fbb525f859105297cafc222e
Drew Zamora <zamoradrew213@gmail.com>
2025-03-19 16:13:03 +0800

    Version 1

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace sampledb;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace sampledb;
+using System.Diagnostics;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnAddUserClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new AddUserPage());
+    }
+
+    private async void OnShowUsersClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ShowUsersPage());
+    }
+}
